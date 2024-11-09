abc398999da0fe85bb79ea0651ddd5c8a5a15ca6
Gnaneshwari Gunasekaran <83358938+Gnaneshwari28@users.noreply.github.com>
2024-11-09 18:56:40 +0000

    Gmap integration changes

--- Resume/src/app/personal-details-card/personal-details-card.component.ts
+++ Resume/src/app/personal-details-card/personal-details-card.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, InjectionToken } from '@angular/core';
+import { GoogleMapsModule } from "@angular/google-maps";
+
 
 @Component({
   selector: 'app-personal-details-card',
   standalone: true,
-  imports: [],
+  imports: [GoogleMapsModule],
   templateUrl: './personal-details-card.component.html',
   styleUrl: './personal-details-card.component.scss'
 })
 export class PersonalDetailsCardComponent {
+
+  options: google.maps.MapOptions = {
+    mapId: "fef1e383c9157d0f",
+    center: { lat: 48.8584, lng: 2.2945 }, 
+    zoom: 15, 
+  };
+ 
 
 }
